addd63127405063c36a956a338cccec29a160646
mlmedina92 <lm30540@gmail.com>
2023-05-03 19:48:27 -0300

    styles AbstractSection

--- martens/src/components/AbstractSection.jsx
+++ martens/src/components/AbstractSection.jsx
@@ -3,20 +3,18 @@ import {  Box, Container,  Typography } from '@mui/material'
 
 export default function AbstractSection() {
     return (
-        <Box>
-                    <Container className="servicios" maxWidth="xl">
-                <div className="nuestrosServicios">
+                <Container className="servicios" maxWidth="xl" sx={{textAlign:'center'}}>
+                <Box>
                     <Typography variant="h3">Somos Fabricantes de aberturas.</Typography>
                     <Typography variant="h3">Realizamos trabajos a medida</Typography>
-                    <Typography variant="body1" className="d-block mb-3 ss">Cerramientos</Typography>
-                    <Typography variant="body1" className="d-block mb-3 ss">Puertas de ingresos automáticas</Typography>
-                    <Typography variant="body1" className="d-block mb-3 ss">Soluciones en chapas</Typography>
-                    <Typography variant="body1" className="d-block mb-3 ss">Escaleras</Typography>
-                    <Typography variant="body1" className="d-block mb-3 ss">Vidrios - espejos - vidrieras</Typography>
-                    <Typography variant="body1" className="d-block mb-3 ss">Trabajos en ciudad y campo</Typography>
-                </div>
+                    <Typography variant="body1">Cerramientos</Typography>
+                    <Typography variant="body1">Puertas de ingresos automáticas</Typography>
+                    <Typography variant="body1">Soluciones en chapas</Typography>
+                    <Typography variant="body1">Escaleras</Typography>
+                    <Typography variant="body1">Vidrios - espejos - vidrieras</Typography>
+                    <Typography variant="body1">Trabajos en ciudad y campo</Typography>
+                </Box>
             </Container>
-        </Box>
     )
 
 }
