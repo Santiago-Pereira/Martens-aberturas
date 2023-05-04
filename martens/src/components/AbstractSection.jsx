b42883b1d1a6b4c9a0d8db0027a8e1cb66a9ebab
mlmedina92 <lm30540@gmail.com>
2023-05-04 09:55:13 -0300

    adress correction

--- martens/src/components/AbstractSection.jsx
+++ martens/src/components/AbstractSection.jsx
@@ -3,16 +3,16 @@ import {  Box, Container,  Typography } from '@mui/material'
 
 export default function AbstractSection() {
     return (
-                <Container className="servicios" maxWidth="xl" sx={{textAlign:'center'}}>
-                <Box>
-                    <Typography variant="h3">Somos Fabricantes de aberturas.</Typography>
-                    <Typography variant="h3">Realizamos trabajos a medida</Typography>
-                    <Typography variant="body1">Cerramientos</Typography>
-                    <Typography variant="body1">Puertas de ingresos automáticas</Typography>
-                    <Typography variant="body1">Soluciones en chapas</Typography>
-                    <Typography variant="body1">Escaleras</Typography>
-                    <Typography variant="body1">Vidrios - espejos - vidrieras</Typography>
-                    <Typography variant="body1">Trabajos en ciudad y campo</Typography>
+                <Container className="servicios" maxWidth="xl" >
+                <Box sx={{color:'whitesmoke',textAlign:'center',display:'flex',flexDirection:"column", justifyContent:'center',alignItems:'center'}}>
+                    <Typography variant="h4">Somos Fabricantes de aberturas.</Typography>
+                    <Typography variant="h4" sx={{paddingBottom:"3rem"}}>Realizamos trabajos a medida</Typography>
+                    <Typography variant="h6">Cerramientos</Typography>
+                    <Typography variant="h6">Puertas de ingresos automáticas</Typography>
+                    <Typography variant="h6">Soluciones en chapas</Typography>
+                    <Typography variant="h6">Escaleras</Typography>
+                    <Typography variant="h6">Vidrios - espejos - vidrieras</Typography>
+                    <Typography variant="h6">Trabajos en ciudad y campo</Typography>
                 </Box>
             </Container>
     )
