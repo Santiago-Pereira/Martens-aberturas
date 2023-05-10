11745e3b01b25a5639c1c6ac28b8768e5304665c
Santiago-Pereira <santiagopereira_8@hotmail.com>
2023-05-10 17:53:41 -0300

    "products pages"

--- martens/src/app/page.js
+++ martens/src/app/page.js
@@ -4,11 +4,9 @@ import Banner from "@/components/Banner";
 import Form from "@/components/Form";
 import Map from "@/components/Map";
 import AbstractSection from "@/components/AbstractSection";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import AOS from "aos";
+import "aos/dist/aos.css";
 AOS.init();
-
-
 
 export default function Home() {
   return (
