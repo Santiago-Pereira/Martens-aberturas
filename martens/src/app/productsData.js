641da8b4c40fbe9254403a02616acf7f31b087b8
mlmedina92 <lm30540@gmail.com>
2023-05-23 17:46:33 -0300

    img news

--- martens/src/app/productsData.js
+++ martens/src/app/productsData.js
@@ -4,211 +4,236 @@ let products = [
     description: "producto de alta calidad",
     category: "puertas",
     id: 0,
-    image: ""
+    image: "./images/PUERTAS/1.jpeg",
+    imageModal: "./images/PUERTAS/chicas/1.jpeg"
   },
   {
     name: "Puerta 2",
     description: "producto de alta calidad",
     category: "puertas",
     id: 1,
-    image: ""
+    image: "./images/PUERTAS/2.jpeg",
+    imageModal: "./images/PUERTAS/chicas/2.jpeg"
   },
   {
     name: "Puerta 3",
     description: "producto de alta calidad",
     category: "puertas",
     id: 2,
-    image: ""
+    image: "./images/PUERTAS/3.jpeg",
+    imageModal: "./images/PUERTAS/chicas/3.jpeg"
   },
   {
     name: "Puerta 4",
     description: "producto de alta calidad",
     category: "puertas",
     id: 3,
-    image: ""
+    image: "./images/PUERTAS/4.jpeg",
+    imageModal: "./images/PUERTAS/chicas/4.jpeg"
   },
   {
     name: "Puert 5",
     description: "producto de alta calidad",
     category: "puertas",
     id: 4,
-    image: ""
+    image: "./images/PUERTAS/5.jpeg",
+    imageModal: "./images/PUERTAS/chicas/5.jpeg"
   },
   {
     name: "Puerta 6",
     description: "producto de alta calidad",
     category: "puertas",
     id: 5,
-    image: ""
+    image: "./images/PUERTAS/6.jpeg",
+    imageModal: "./images/PUERTAS/chicas/6.jpeg"
   },
   {
     name: "Puerta 7",
     description: "producto de alta calidad",
     category: "puertas",
     id: 6,
-    image: ""
+    image: "./images/PUERTAS/7.jpeg",
+    imageModal: "./images/PUERTAS/chicas/7.jpeg"
   },
   {
     name: "Puerta 8",
     description: "producto de alta calidad",
     category: "puertas",
     id: 7,
-    image: ""
+    image: "./images/PUERTAS/8.jpeg", 
+    imageModal: "./images/PUERTAS/chicas/8.jpeg"
   },
   {
     name: "Puerta 9",
     description: "producto de alta calidad",
     category: "puertas",
     id: 8,
-    image: ""
+    image: "./images/PUERTAS/9.jpeg", 
+    imageModal: "./images/PUERTAS/chicas/9.jpeg"
   },
   {
     name: "Puerta 10",
     description: "producto de alta calidad",
     category: "puertas",
     id: 9,
-    image: ""
+    image: "./images/PUERTAS/10.jpeg",
+    imageModal: "./images/PUERTAS/chicas/10.jpeg"
   },
   {
     name: "Puerta 11",
     description: "producto de alta calidad",
     category: "puertas",
     id: 10,
-    image: ""
+    image: "./images/PUERTAS/11.jpeg",
+    imageModal: "./images/PUERTAS/chicas/11.jpeg"
   },
   {
     name: "Puerta 12",
     description: "producto de alta calidad",
     category: "puertas",
     id: 11,
-    image: ""
+    image: "./images/PUERTAS/12.jpeg", 
+    imageModal: "./images/PUERTAS/chicas/12.jpeg"
   },
   {
     name: "porton 1",
     description: "producto de alta calidad",
     category: "portones",
     id: 12,
-    image: ""
+    image: "./images/porton/1.jpeg",
+    imageModal: "./images/porton/chicas/1.jpeg"
   },
   {
     name: "porton 2",
     description: "producto de alta calidad",
     category: "portones",
     id: 13,
-    image: ""
+    image: "./images/porton/2.jpeg", 
+    imageModal: "./images/porton/chicas/2.jpeg"
   },
   {
     name: "porton 3",
     description: "producto de alta calidad",
     category: "portones",
     id: 14,
-    image: ""
+    image: "./images/porton/3.jpeg", 
+    imageModal: "./images/porton/chicas/3.jpeg"
   },
   {
     name: "porton 4",
     description: "producto de alta calidad",
     category: "portones",
     id: 15,
-    image: ""
+    image: "./images/porton/4.jpeg", 
+    imageModal: "./images/porton/chicas/4.jpeg"
   },
   {
     name: "porton 5",
     description: "producto de alta calidad",
     category: "portones",
     id: 16,
-    image: ""
+    image: "./images/porton/5.jpeg", 
+    imageModal: "./images/porton/chicas/5.jpeg"
   },
   {
     name: "porton 6",
     description: "producto de alta calidad",
     category: "portones",
     id: 17,
-    image: ""
+    image: "./images/porton/6.jpeg", 
+    imageModal: "./images/porton/chicas/6.jpeg"
   },
   {
     name: "ventana 1",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 18,
-    image: ""
+    image: "./images/VENTANA/1.jpeg",
+    imageModal: "./images/chicas/VENTANA/1.jpeg"
   },
   {
     name: "ventana 2",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 19,
-    image: ""
+    image: "./images/VENTANA/2.jpeg",
+    imageModal: "./images/chicas/VENTANA/2.jpeg"
   },
   {
     name: "ventana 3",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 20,
-    image: ""
+    image: "./images/VENTANA/3.jpeg",
+    imageModal: "./images/chicas/VENTANA/3.jpeg"
   },
   {
     name: "ventana 4",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 21,
-    image: ""
-  },
+    image: "./images/VENTANA/4.jpeg",
+    imageModal: "./images/chicas/VENTANA/4.jpeg"  },
   {
     name: "ventana 5",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 22,
-    image: ""
-  },
+    image: "./images/VENTANA/5.jpeg",
+    imageModal: "./images/chicas/VENTANA/5.jpeg"  },
   {
     name: "ventana 6",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 23,
-    image: ""
+    image: "./images/VENTANA/6.jpeg",
+    imageModal: "./images/chicas/VENTANA/6.jpeg"
   },
   {
     name: "ventana 7",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 24,
-    image: ""
-  },
+    image: "./images/VENTANA/7.jpeg",
+    imageModal: "./images/chicas/VENTANA/7.jpeg"  },
   {
     name: "ventana 8",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 25,
-    image:
-      ""
+    image: "./images/VENTANA/8.jpeg",
+    imageModal: "./images/chicas/VENTANA/8.jpeg"
   },
   {
     name: "ventana 9",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 26,
-    image: ""
+    image: "./images/VENTANA/9.jpeg",
+    imageModal: "./images/chicas/VENTANA/9.jpeg"
   },
   {
     name: "ventana 10",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 27,
-    image: ""
+    image: "", imageModal: ""
   },
   {
     name: "ventana 11",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 28,
-    image: ""
+    image: "./images/VENTANA/11.jpeg",
+    imageModal: "./images/chicas/VENTANA/11.jpeg"
   },
   {
     name: "ventana 12",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 29,
-    image: ""
+    image: "./images/VENTANA/12.jpeg",
+    imageModal: "./images/chicas/VENTANA/12.jpeg"
   },
 
   {
@@ -216,42 +241,43 @@ let products = [
     description: "producto de alta calidad",
     category: "pergolas",
     id: 30,
-    image: ""
+    image: "", imageModal: ""
   },
   {
     name: "Escalera 2",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 31,
-    image: ""
+    image: "", imageModal: ""
   },
   {
     name: "Escalera 3",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 32,
-    image: ""
+    image: "", imageModal: ""
   },
   {
     name: "Escalera 4",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 33,
-    image: ""
+    image: "", imageModal: ""
   },
   {
     name: "Escalera 5",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 34,
-    image: ""
+    image: "", imageModal: ""
   },
   {
     name: "Escalera 6",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 35,
-    image: ""
+    image: "",
+    imageModal: ""
   },
 
   {
@@ -259,21 +285,22 @@ let products = [
     description: "producto de alta calidad",
     category: "pergolas",
     id: 36,
-    image: ""
+    image: "", imageModal: ""
   },
   {
     name: "Pergola  2",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 37,
-    image: ""
+    image: "", imageModal: ""
   },
   {
     name: "Pergola  3",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 38,
-    image: ""
+    image: "",
+     imageModal: ""
   },
 
   {
@@ -281,105 +308,120 @@ let products = [
     description: "producto de alta calidad",
     category: "trabajos",
     id: 39,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
   {
     name: "hierro 2",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 40,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
   {
     name: "hierro 3",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 41,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
   {
     name: "hierro 4",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 42,
-    image: ""
+    image: "",
+    imageModal: ""
   },
   {
     name: "hierro 5",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 43,
-    image: ""
+    image: "",
+    imageModal: ""
   },
   {
     name: "hierro 6",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 44,
-    image: ""
+    image: "",
+    imageModal: ""
   },
   {
     name: "soluciones 1",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 45,
-    image: ""
+    image: "",
+    imageModal: ""
   },
   {
     name: "soluciones 2",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 46,
-    image: ""
+    image: "",
+    imageModal: ""
   },
   {
     name: "soluciones 3",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 47,
-    image: ""
+    image: "",
+    imageModal: ""
   },
   {
     name: "soluciones 4",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 48,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
   {
     name: "soluciones 5",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 49,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
   {
     name: "soluciones 6",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 50,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
   {
     name: "soluciones 7",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 51,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
   {
     name: "soluciones 8",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 52,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
   {
     name: "soluciones 9",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 53,
-    image: ""
+    image: "", 
+    imageModal: ""
   },
 ];
 export default products;
