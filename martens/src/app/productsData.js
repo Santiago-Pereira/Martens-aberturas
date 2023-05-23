02b9dd3924862ab7ee638e125a13fee0767d5f9c
mlmedina92 <lm30540@gmail.com>
2023-05-23 16:14:47 -0300

    delete url cloudinary

--- martens/src/app/productsData.js
+++ martens/src/app/productsData.js
@@ -4,200 +4,175 @@ let products = [
     description: "producto de alta calidad",
     category: "puertas",
     id: 0,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791922/martens/puertas/1_zyddxy.jpg",
+    image: ""
   },
   {
     name: "Puerta 2",
     description: "producto de alta calidad",
     category: "puertas",
     id: 1,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791921/martens/puertas/WhatsApp_Image_2023-05-19_at_3.53.35_PM_1_oclqgv.jpg",
+    image: ""
   },
   {
     name: "Puerta 3",
     description: "producto de alta calidad",
     category: "puertas",
     id: 2,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684793181/martens/Hierros/WhatsApp_Image_2023-05-22_at_7.05.24_PM_1_yau7xl.jpg",
+    image: ""
   },
   {
     name: "Puerta 4",
     description: "producto de alta calidad",
     category: "puertas",
     id: 3,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791921/martens/puertas/WhatsApp_Image_2023-05-19_at_3.53.35_PM_nwuxyr.jpg",
+    image: ""
   },
   {
     name: "Puert 5",
     description: "producto de alta calidad",
     category: "puertas",
     id: 4,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791921/martens/puertas/2_ocykvn.jpg",
+    image: ""
   },
   {
     name: "Puerta 6",
     description: "producto de alta calidad",
     category: "puertas",
     id: 5,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791921/martens/puertas/WhatsApp_Image_2023-05-18_at_7.07.58_PM_frnt2g.jpg",
+    image: ""
   },
   {
     name: "Puerta 7",
     description: "producto de alta calidad",
     category: "puertas",
     id: 6,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791921/martens/puertas/WhatsApp_Image_2023-05-18_at_7.02.32_PM_gyje8k.jpg",
+    image: ""
   },
   {
     name: "Puerta 8",
     description: "producto de alta calidad",
     category: "puertas",
     id: 7,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791921/martens/puertas/WhatsApp_Image_2023-05-18_at_4.52.18_PM_tzfex1.jpg",
+    image: ""
   },
   {
     name: "Puerta 9",
     description: "producto de alta calidad",
     category: "puertas",
     id: 8,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791920/martens/puertas/3_rxzomd.jpg",
+    image: ""
   },
   {
     name: "Puerta 10",
     description: "producto de alta calidad",
     category: "puertas",
     id: 9,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791920/martens/puertas/5_qolt53.jpg",
+    image: ""
   },
   {
     name: "Puerta 11",
     description: "producto de alta calidad",
     category: "puertas",
     id: 10,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791921/martens/puertas/7_zdibyh.jpg",
+    image: ""
   },
   {
     name: "Puerta 12",
     description: "producto de alta calidad",
     category: "puertas",
     id: 11,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791920/martens/puertas/WhatsApp_Image_2023-05-18_at_4.52.09_PM_yjcess.jpg",
+    image: ""
   },
   {
     name: "porton 1",
     description: "producto de alta calidad",
     category: "portones",
     id: 12,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790222/martens/porton/5_fhnxxu.jpg",
+    image: ""
   },
   {
     name: "porton 2",
     description: "producto de alta calidad",
     category: "portones",
     id: 13,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790222/martens/porton/porton3_ag9kce.jpg",
+    image: ""
   },
   {
     name: "porton 3",
     description: "producto de alta calidad",
     category: "portones",
     id: 14,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790221/martens/porton/porton2_jgghde.jpg",
+    image: ""
   },
   {
     name: "porton 4",
     description: "producto de alta calidad",
     category: "portones",
     id: 15,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790221/martens/porton/porton4_stllxe.jpg",
+    image: ""
   },
   {
     name: "porton 5",
     description: "producto de alta calidad",
     category: "portones",
     id: 16,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790221/martens/porton/WhatsApp_Image_2023-05-19_at_4.03.18_PM_1_iohpae.jpg",
+    image: ""
   },
   {
     name: "porton 6",
     description: "producto de alta calidad",
     category: "portones",
     id: 17,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790221/martens/porton/porton_ipryl8.jpg",
+    image: ""
   },
   {
     name: "ventana 1",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 18,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790912/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.07.59_PM_1_fcm4fv.jpg",
+    image: ""
   },
   {
     name: "ventana 2",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 19,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790912/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.08.04_PM_x73hu2.jpg",
+    image: ""
   },
   {
     name: "ventana 3",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 20,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790912/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.08.04_PM_1_e0uhge.jpg",
+    image: ""
   },
   {
     name: "ventana 4",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 21,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790912/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.07.56_PM_1_e6ntze.jpg",
+    image: ""
   },
   {
     name: "ventana 5",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 22,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790912/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.07.55_PM_neulgi.jpg",
+    image: ""
   },
   {
     name: "ventana 6",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 23,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790912/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.07.55_PM_2_duwjko.jpg",
+    image: ""
   },
   {
     name: "ventana 7",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 24,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790912/martens/Ventana/2_zmyltb.jpg",
+    image: ""
   },
   {
     name: "ventana 8",
@@ -205,39 +180,35 @@ let products = [
     category: "ventanas",
     id: 25,
     image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790912/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.07.54_PM_xbiafs.jpg",
+      ""
   },
   {
     name: "ventana 9",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 26,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790911/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.08.08_PM_1_nfzvok.jpg",
+    image: ""
   },
   {
     name: "ventana 10",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 27,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790911/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.08.05_PM_1_vqxgzr.jpg",
+    image: ""
   },
   {
     name: "ventana 11",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 28,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684790911/martens/Ventana/WhatsApp_Image_2023-05-18_at_7.08.09_PM_1_bvvrdx.jpg",
+    image: ""
   },
   {
     name: "ventana 12",
     description: "producto de alta calidad",
     category: "ventanas",
     id: 29,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684792690/martens/Ventana/agregar_cytjdy.jpg",
+    image: ""
   },
 
   {
@@ -245,48 +216,42 @@ let products = [
     description: "producto de alta calidad",
     category: "pergolas",
     id: 30,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791121/martens/Escalera/1_lpefkz.jpg",
+    image: ""
   },
   {
     name: "Escalera 2",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 31,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791121/martens/Escalera/2_tzqjhb.jpg",
+    image: ""
   },
   {
     name: "Escalera 3",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 32,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791120/martens/Escalera/7_kj5a8f.jpg",
+    image: ""
   },
   {
     name: "Escalera 4",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 33,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791120/martens/Escalera/4_lkjt6x.jpg",
+    image: ""
   },
   {
     name: "Escalera 5",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 34,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791120/martens/Escalera/5_ftinz4.jpg",
+    image: ""
   },
   {
     name: "Escalera 6",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 35,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791120/martens/Escalera/3_m6evt8.jpg",
+    image: ""
   },
 
   {
@@ -294,24 +259,21 @@ let products = [
     description: "producto de alta calidad",
     category: "pergolas",
     id: 36,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791121/martens/Escalera/10_g1hlf2.jpg",
+    image: ""
   },
   {
     name: "Pergola  2",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 37,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791121/martens/Escalera/9_emoyc8.jpg",
+    image: ""
   },
   {
     name: "Pergola  3",
     description: "producto de alta calidad",
     category: "pergolas",
     id: 38,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684791120/martens/Escalera/7_kj5a8f.jpg",
+    image: ""
   },
 
   {
@@ -319,120 +281,105 @@ let products = [
     description: "producto de alta calidad",
     category: "trabajos",
     id: 39,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789644/martens/Hierros/d_bxlk2s.jpg",
+    image: ""
   },
   {
     name: "hierro 2",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 40,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789644/martens/Hierros/b_kfer40.jpg",
+    image: ""
   },
   {
     name: "hierro 3",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 41,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789644/martens/Hierros/a_huvw3m.jpg",
+    image: ""
   },
   {
     name: "hierro 4",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 42,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789644/martens/Hierros/f_nk7pso.jpg",
+    image: ""
   },
   {
     name: "hierro 5",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 43,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789644/martens/Hierros/cc_d5oxly.jpg",
+    image: ""
   },
   {
     name: "hierro 6",
     description: "producto de alta calidad",
     category: "trabajos",
     id: 44,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789644/martens/Hierros/e_lo2e0p.jpg",
+    image: ""
   },
   {
     name: "soluciones 1",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 45,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/2_hdg6hy.jpg",
+    image: ""
   },
   {
     name: "soluciones 2",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 46,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/1_mn8obj.jpg",
+    image: ""
   },
   {
     name: "soluciones 3",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 47,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/WhatsApp_Image_2023-05-18_at_7.02.37_PM_pnfwfn.jpg",
+    image: ""
   },
   {
     name: "soluciones 4",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 48,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/8_bu9al5.jpg",
+    image: ""
   },
   {
     name: "soluciones 5",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 49,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/WhatsApp_Image_2023-05-18_at_7.02.37_PM_1_eauzlx.jpg",
+    image: ""
   },
   {
     name: "soluciones 6",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 50,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/WhatsApp_Image_2023-05-18_at_4.52.04_PM_tclspv.jpg",
+    image: ""
   },
   {
     name: "soluciones 7",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 51,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/WhatsApp_Image_2023-05-18_at_4.52.04_PM_1_asljvm.jpg",
+    image: ""
   },
   {
     name: "soluciones 8",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 52,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/WhatsApp_Image_2023-05-18_at_4.49.42_PM_iemetv.jpg",
+    image: ""
   },
   {
     name: "soluciones 9",
     description: "producto de alta calidad",
     category: "soluciones-generales",
     id: 53,
-    image:
-      "https://res.cloudinary.com/dmatgvjjy/image/upload/v1684789908/martens/Soluciones%20grales/WhatsApp_Image_2023-05-18_at_4.52.05_PM_zrzrth.jpg",
+    image: ""
   },
 ];
 export default products;
