812a778c7b4d7fdd1464bfe217a8072b09566b77
Santiago-Pereira <santiagopereira_8@hotmail.com>
2023-05-29 17:22:43 -0300

    "gfhf"

--- martens/src/app/page.js
+++ martens/src/app/page.js
@@ -4,14 +4,11 @@ import Banner from "@/components/Banner";
 import Form from "@/components/Form";
 import Map from "@/components/Map";
 import AbstractSection from "@/components/AbstractSection";
-/* import AOS from "aos";
-import "aos/dist/aos.css"; */
 import Footer from "@/components/Footer";
 import WhatsAppBtn from "@/components/WhatsAppBtn";
 import Copyright from "@/components/Copyright";
 
 export default function Home() {
-  /*   AOS.init(); */
   return (
     <>
       <main>
