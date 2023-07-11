3a081a65f8dc0fb6c680dc566f30d701ab1da845
mlmedina92 <lm30540@gmail.com>
2023-07-11 22:01:35 +0200

    Remove useRouter

--- martens/src/app/[category]/page.jsx
+++ martens/src/app/[category]/page.jsx
@@ -1,20 +1,13 @@
 "use client";
 import dynamic from "next/dynamic";
 import { usePathname } from "next/navigation";
-import { useRouter } from "next/router";
 
 function Page() {
-  const router = useRouter();
-
   const Prods = dynamic(() => import("@/components/ProductsCard"), {
     ssr: false,
   });
 
   const pathname = usePathname();
-
-  if (router.isFallback) {
-    return <h2>Cargando...</h2>;
-  }
 
   return <Prods category={pathname.replace("/", "")} />;
 }
@@ -27,6 +20,6 @@ export function getStaticPaths() {
   const paths = categoriesData.map((cat) => ({
     params: { id: cat.code },
   }));
-
+  
   return { paths, fallback: true };
 }
