a826e7b467f0dda6af6139e5fb9b54040bc66246
mlmedina92 <lm30540@gmail.com>
2023-07-11 20:57:00 +0200

    Remove commented code

--- martens/src/app/[category]/page.jsx
+++ martens/src/app/[category]/page.jsx
@@ -23,25 +23,25 @@ export async function getStaticProps() {
 }
 
 export async function getStaticPaths() {
-  // // When this is true (in preview environments) don't
-  // // prerender any static pages
-  // // (faster builds, but slower initial page load)
-  // if (process.env.SKIP_BUILD_STATIC_GENERATION) {
-  //   return {
-  //     paths: [],
-  //     fallback: 'blocking',
-  //   };
-  // }
+  // When this is true (in preview environments) don't
+  // prerender any static pages
+  // (faster builds, but slower initial page load)
+  if (process.env.SKIP_BUILD_STATIC_GENERATION) {
+    return {
+      paths: [],
+      fallback: 'blocking',
+    };
+  }
 
-  // // Get the paths we want to prerender based on posts
-  // // In production environments, prerender all pages
-  // // (slower builds, but faster initial page load)
-  // const paths = categoriesData.map((cat) => ({
-  //   params: { id: cat.code },
-  // }));
+  // Get the paths we want to prerender based on posts
+  // In production environments, prerender all pages
+  // (slower builds, but faster initial page load)
+  const paths = categoriesData.map((cat) => ({
+    params: { id: cat.code },
+  }));
 
-  // // { fallback: false } means other routes should 404
-  // return { paths, fallback: false };
+  // { fallback: false } means other routes should 404
+  return { paths, fallback: false };
 
-  return { paths: [], fallback: true };
+  // return { paths: [], fallback: true };
 }
