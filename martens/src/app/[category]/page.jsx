96430f6340483bef7afebcf36a5163fb89c18948
mlmedina92 <lm30540@gmail.com>
2023-07-11 21:19:07 +0200

    Remove getStatisPaths from category page

--- martens/src/app/[category]/page.jsx
+++ martens/src/app/[category]/page.jsx
@@ -16,32 +16,26 @@ export default Page;
 
 import categoriesData from "../categoriesData.js";
 
-// export async function getStaticProps() {
-//   return {
-//     props: {},
-//   };
+// export async function getStaticPaths() {
+//   // When this is true (in preview environments) don't
+//   // prerender any static pages
+//   // (faster builds, but slower initial page load)
+//   if (process.env.SKIP_BUILD_STATIC_GENERATION) {
+//     return {
+//       paths: [],
+//       fallback: 'blocking',
+//     };
+//   }
+
+//   // Get the paths we want to prerender based on posts
+//   // In production environments, prerender all pages
+//   // (slower builds, but faster initial page load)
+//   const paths = categoriesData.map((cat) => ({
+//     params: { id: cat.code },
+//   }));
+
+//   // { fallback: false } means other routes should 404
+//   return { paths, fallback: false };
+
+//   // return { paths: [], fallback: true };
 // }
-
-export async function getStaticPaths() {
-  // When this is true (in preview environments) don't
-  // prerender any static pages
-  // (faster builds, but slower initial page load)
-  if (process.env.SKIP_BUILD_STATIC_GENERATION) {
-    return {
-      paths: [],
-      fallback: 'blocking',
-    };
-  }
-
-  // Get the paths we want to prerender based on posts
-  // In production environments, prerender all pages
-  // (slower builds, but faster initial page load)
-  const paths = categoriesData.map((cat) => ({
-    params: { id: cat.code },
-  }));
-
-  // { fallback: false } means other routes should 404
-  return { paths, fallback: false };
-
-  // return { paths: [], fallback: true };
-}
